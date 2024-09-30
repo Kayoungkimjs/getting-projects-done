4e9282ab6c77b2ec7685cf20584580d5e3b74b69
Kim Kayoung <kykim.dev@gmail.com>
2024-09-30 14:23:28 +0900

    🔧 Add graphql schema files and set config modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ProjectModule } from './modules/project.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { rootResolvers } from './resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProjectModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'gpd.sqlite',
+      autoLoadEntities: true,
+      synchronize: false,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: { path: '../grapql/schema.graphql' },
+    }),
+  ],
+
+  providers: [...rootResolvers],
 })
 export class AppModule {}
