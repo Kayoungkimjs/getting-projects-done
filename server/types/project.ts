15481efb07982d8cb4d47b56675b887b7b829c89
Kim Kayoung <kykim.dev@gmail.com>
2024-09-30 14:55:55 +0900

    🏷️ Add Project interface types

---
+++ server/types/project.ts
@@ -0,0 +1,14 @@
+export interface Project {
+  id: number;
+  categoryId: number;
+  categoryKr: string;
+  categoryEn: string;
+  endpoint: string;
+  testType: string;
+  testName: string;
+  criteria: string;
+  result: number;
+  status: number;
+  priority: number;
+  notes: string;
+}
