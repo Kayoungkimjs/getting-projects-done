79c1f7c3bd6a46a38b25637904835b97b1ec49fb
Kim Kayoung <kykim.dev@gmail.com>
2024-10-02 15:53:15 +0900

    🔧 Update CORS settings

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:5174',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(4000);
 }
 bootstrap();
