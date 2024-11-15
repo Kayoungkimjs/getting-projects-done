56f4b565ff131f5ebe0d8f326e943a0a664c5b53
Kim Kayoung <kykim.dev@gmail.com>
2024-11-15 14:07:54 +0900

    📝 Add client graphql config

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,13 +1,43 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
 
 const SERVER_URL = "http://localhost:4000/graphql";
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: SERVER_URL,
+});
+
+// Middleware to attach the token to the headers
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    operation.setContext({
+      headers: {
+        ...operation.getContext().headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  }
+
+  return forward(operation);
+});
+
+// Combine the links
+const link = authLink.concat(httpLink);
 
 export const gqlCache = new InMemoryCache({
   addTypename: false,
 });
 
 export const apolloClient = new ApolloClient({
-  uri: SERVER_URL,
+  link,
   cache: gqlCache,
+  credentials: "include",
   connectToDevTools: true,
 });
