62e25ab8bddc2e181225612d6be1d9cb4738447b
Kim Kayoung <kykim.dev@gmail.com>
2024-12-08 11:41:36 +0900

    Set export index of component and pages

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,1 +1,7 @@
 export { Login } from "./Login/Login";
+export { GpdBoard } from "./Main/GpdBoard";
+export { Greeting } from "./Entry/Gretting";
+export { NameForm } from "./Entry/NameForm";
+export { SelectCategoryForm } from "./Entry/SelectCategoryForm";
+export { SelectProjectForm } from "./Entry/SelectProjectForm";
+export { Main } from "./Main/index";
