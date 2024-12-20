d20b811475c5d00b0003263daf5273a06a1914f1
Kim Kayoung <kykim.dev@gmail.com>
2024-12-20 19:19:12 +0900

    Add gpd board page

---
+++ client/src/pages/Main/GpdForm.tsx
@@ -0,0 +1,31 @@
+import { useForm } from "react-hook-form";
+import { StyledGpdForm, StyledOverlay } from "./style";
+
+export const GpdForm: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <StyledGpdForm>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>Creteria</label>
+            <input
+              {...register("firstName", { required: true, maxLength: 20 })}
+            />
+          </div>
+          <div>
+            <label>priority</label>
+            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+          </div>
+          <div>
+            <input type="number" {...register("age", { min: 18, max: 99 })} />
+          </div>
+          <input type="submit" />
+        </form>
+      </StyledGpdForm>
+      <StyledOverlay />
+    </>
+  );
+};
